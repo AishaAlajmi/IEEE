d111b76076dd02ada993ef6b028f13399c8bc0b0
AishaAlajmi <105488603+AishaAlajmi@users.noreply.github.com>
2025-01-06 05:15:35 +0300

    Add files via upload

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,16 +3,28 @@ import Img2 from '../images/Rectangle 10.png';
 import MEDIA from '../images/Ellipse 15.png';
 import PUBLICITY from '../images/Ellipse 13.png';
 import EXECUTION from '../images/Ellipse 11.png';
+import con from '../images/Group 29.png';
+import cre from '../images/Group 30.png';
+import Collaboration from '../images/Group 31.png';
+import soci from '../images/Group 32.png';
+import SectionArrow from '../images/Polygon 7.png';
+
 export const Bio = {
   name: "Welcome to IEEE Club!",
   description:
     "The world's largest professional organization promotes innovation and technological excellence for humanity. We empower students in technical fields and embrace modern concepts for effective technological advancement and societal progress.",
   github: "https://github.com/AishaAlajmi",
-  resume:
-    "https://drive.google.com/file/d",
-  linkedin: "https://www.linkedin.com/in/Aisha-Alajmi/",
-  twitter: "https://twitter.com/Aiisha_h17",
+
+  linkedin: "https://www.linkedin.com/in/",
+  twitter: "https://twitter.com/",
 };
+// Core values of the organization
+export const CoreValuesList = [
+  { id: 1, title: "Continuous Evolution", image: con },
+  { id: 2, title: "Creativity", image: cre },
+  { id: 3, title: "Collaboration", image: Collaboration },
+  { id: 4, title: "Social Responsibility", image: soci },
+];
 
 export const Gallary = [
   {
@@ -84,7 +96,7 @@ export const projects = [
     title: "KAUST Trip",
     description:
       "Ifsah is an integrated accounting solution that enhances financial transparency by ensuring accurate disclosures of transactions with related parties, customers, and suppliers. It automates financial statement generation, ensures compliance with built-in alerts and approvals, and features a smart digital assistant to support accountants in making informed decisions and improving company performance.",
-      image: Img2,
+    image: Img2,
 
   },
   {
@@ -92,7 +104,7 @@ export const projects = [
     title: "Engineering Day 2024",
     description:
       "Anis is an AI-powered intelligent character designed to improve psychological therapy. It analyzes the patient's psychological and medical data, as well as their social media activity (with the patient's consent), to provide personalized consultations that predict psychological crises before they occur, enabling early intervention and appropriate support. Anis enhances communication between patients and specialists, raises patients' awareness of their condition, improving the quality of care while reducing costs and the number of therapy sessions.",
-      image: Img1,
+    image: Img1,
 
 
   },
